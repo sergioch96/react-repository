c9b41a73adc1b565af64c65273ce062b92f00912
Sergio Chamorro <sergiochamo96@gmail.com>
2025-04-10 21:03:41 -0300

    update mouse follow

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from "react";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  // pointer move
+  useEffect(() => {
+    console.log("efect", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      console.log("cleanup", { enabled });
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  // change body class
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button
+        onClick={() => {
+          setEnabled(!enabled);
+        }}
+      >
+        {enabled ? "Disable" : "Enable"} mouse follow
+      </button>
+    </>
+  );
+};
+
 function App() {
-  return <h3>Project 3</h3>;
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  );
 }
 
 export default App;
